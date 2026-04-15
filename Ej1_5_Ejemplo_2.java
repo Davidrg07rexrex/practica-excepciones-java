6c935baca3ce1cc9c1b3cf5c5a4918af27cf74f0
Davidrg07rexrex <david.ruizgil@edu.uah.es>
2026-04-15 17:57:34 +0200

    Delete Ej1_5_Ejemplo_2.java

--- Ej1_5_Ejemplo_2.java
+++
@@ -1,9 +0,0 @@
-public class Ej1_5_Ejemplo_2 {
-        public void methodD() throws Exception { //igual que antes
-            throw new Exception("otro error"); //lanza el fallo
-        }
-
-        public void methodC() throws Exception { //la clave aqui es volver a poner throws
-            methodD(); //como avisamos en la firma no hace falta try-catch
-        }
-}
